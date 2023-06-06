f12ad9769dca1bcab0b7ba948278b217ecdaf0ca
Romoramel <romoramel@hotmail.com>
2023-06-06 11:58:13 -0700

    api

--- app/controllers/mgames_controller.rb
+++ app/controllers/mgames_controller.rb
@@ -1,2 +1,23 @@
 class MgamesController < ApplicationController
+        def index
+            mgames = Mgame.all
+            render json: mgames
+        end
+      
+        def create
+        # Create a new cat
+            mgame = Mgame.create(mgame_params)
+            render json: mgame
+        end
+      
+        def update
+        end
+      
+        def destroy
+        end
+
+        private
+        def mgame_params
+          params.require(:mgame).permit(:name, :game_type, :file_size, :summary, :img,)
+        end
 end
