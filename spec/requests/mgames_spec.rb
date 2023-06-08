1107fcb138179b404519a2e351505b5bfa1bbd21
LEARN Academy <lanyuke@hotmail.com>
2023-06-08 10:24:20 -0700

    fix commet out code

--- spec/requests/mgames_spec.rb
+++ spec/requests/mgames_spec.rb
@@ -142,32 +142,4 @@ RSpec.describe "Mgames", type: :request do
 
   end
 
-  # describe "POST /create" do
-  #   it "creates a mgame" do
-  #     # The params we are going to send with the request
-  #     mgame_params = {
-  #       mgame: {
-  #         name: 'Blue archive',
-  #         game_type: 'Tactical Role-Playing',
-  #         summary: 'Blue Archive is a tactical role-playing game that allows the player to form and mobilize units of up to six members two \"Specials\" and four \"Strikers\" to participate in various military campaigns with. Students strengths can be enhanced in various ways, such as by increasing their levels, weapons, armor and skills.',
-  #         img: 'https://media.tenor.com/Z6EFYrE_GJkAAAAd/blue-archive-arisu.gif',
-  #       }
-  #     }
-  
-  #     # Send the request to the server
-  #     post '/mgames', params: mgame_params
-  
-  #     # Assure that we get a success back
-  #     expect(response).to have_http_status(200)
-  
-  #     # Look up the cat we expect to be created in the db
-  #     test= Mgame.first
-  
-  #     # Assure that the created cat has the correct attributes
-  #     expect(test.name).to eq 'Blue archive'
-  #   end
-  # end
-
-  
-
 end
